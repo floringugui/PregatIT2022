a928c6f24d8d4d2ff48fce73354324cf1440796b
Florin GUGUI <fgugui@pentalog.com>
2022-11-21 08:41:07 +0200

    Commit 3

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,59 @@
+﻿//using System.Text;
+
+//var b = new System.Boolean();
+//System.Object i = new int();
+//char c = 'c';
+
+using System.Text;
+
+string cStr = "cccc";
+//cStr = cStr + "d";
+//cStr += "aaaa" + "bbbb";
+
+//Console.WriteLine(cStr);
+
+//var newStr = cStr.Replace("d", string.Empty);
+
+//Console.WriteLine(newStr);
+
+StringBuilder sb = new StringBuilder(cStr);
+sb.Append("d");
+sb.Append("aaaa");
+sb.Append("bbbb");
+
+Console.WriteLine(sb.ToString());
+
+sb.Replace("d", string.Empty);
+
+Console.WriteLine(sb.ToString());
+
+//Console.WriteLine(b.ToString());
+
+//Console.WriteLine("Hello, World!" + System.Environment.NewLine + "Hello, World2!");
+//Console.Beep();
+
+//Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("ro-RO");
+//Console.WriteLine("{0:c}", 1000);
+//var demo = new Demo();
+//demo.LocalVars(i);
+
+//public class Demo : Object
+//{
+//    private int var;
+
+//    public int LocalVars(object parametru)
+//    {
+//        System.Boolean localVar = true;
+//        var localVar2 = 100;
+//        var localVar3 = "Test";
+
+//        parametru.Equals(5);
+
+//        var = 5;
+//        parametru = 5;
+//        localVar = false;
+//        return 0;
+//    }
+
+//    public int Prop { get; set; }
+//}
